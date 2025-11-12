823363c4035571a74174d4e26400f4c6c657a9e8
str7pd <167695696+str7pd@users.noreply.github.com>
2025-11-12 19:02:16 +0330

    Update index.js

--- index.js
+++ index.js
@@ -204,10 +204,7 @@ app.post("/mobile/verifyToken", async (req, res) => {
   uid,
   message: "Login successful"
 });
-   catch (err) {
-    console.error("❌ verifyToken failed:", err);
-    res.json({ status: "error", message: err.message });
-});
+
 
 // Endpoint to verify session (optional)
 app.post("/verify-session", async (req, res) => {
